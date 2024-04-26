9dbd86de7758da66a0f947093379299f115a6b7a
Phillip Schichtel <phillip@schich.tel>
2024-04-26 02:13:44 +0200

    attempt to correctly process multiple ports

--- pgrok.sh
+++ pgrok.sh
@@ -5,17 +5,22 @@ set -euo pipefail
 source "$HOME/.profile"
 
 current_pid="$$"
-parent_pid="$(cat "/proc/${current_pid}/status" | grep PPid | tr -d '\t' | cut -d':' -f2)"
+parent_pid="$(grep PPid "/proc/${current_pid}/status" | tr -d '\t' | cut -d':' -f2)"
 
-forwarded_port="$(bc <<< "ibase=16; $(cat "/proc/${parent_pid}/net/tcp" | grep ': 00000000' | grep -v ': 00000000:0400' | cut -d' ' -f5 | cut -d':' -f2)")"
+readarray -t forwarded_ports <<< "$(bc <<< "ibase=16; $(grep ': 00000000' "/proc/${parent_pid}/net/tcp" | grep -v ': 00000000:0400' | cut -d' ' -f5 | cut -d':' -f2)")"
 
-if [ -z "$forwarded_port" ]
+echo "Ports: ${#forwarded_ports}"
+if [ "${#forwarded_ports}" = 0 ]
 then
     echo "No port forwarded!" >&2
     exit 1
+elif [ "${#forwarded_ports}" -gt 2 ]
+then
+    echo "A maximum of 2 ports may be forward!" >&2
+    exit 1
 fi
 
-echo "Forwarded port: $forwarded_port"
+echo "Forwarded port: ${forwarded_ports[*]}"
 
 command="$SSH_ORIGINAL_COMMAND"
 echo "Original command: $command"
